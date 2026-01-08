8d8c27001bfc9540008b3c96854d64adee7a74ae
alex Kochens <ofwonoalex256@gmail.com>
2026-01-08 10:01:57 +0300

    feat: Learnt State

--- src/components/starProject/Star.jsx
+++ src/components/starProject/Star.jsx
@@ -1,15 +1,21 @@
+import { useState } from 'react';
+
 const Star = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
   const stars = [1, 2, 3, 4, 5];
-  const clicked = (index) => {
-    console.log(`A star  has been clicked at index`, index);
-  };
   return (
     <div className='paragraph'>
       <p className='h1style'>Rate your Experience</p>
 
       <ul>
-        {stars.map((star, index) => (
-          <span onClick={() => clicked(index)} key={star} className='star-span'>
+        {stars.map((star) => (
+          <span
+            onClick={() => setRating(star)}
+            onMouseEnter={() => setHover(star)}
+            key={star}
+            className='star-span'
+          >
             {'\u2605'}
           </span>
         ))}
