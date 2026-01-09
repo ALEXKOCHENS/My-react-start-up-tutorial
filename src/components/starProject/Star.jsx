6832ddb988cae9a5856ef54c3fcc7e76bab27bbf
alex Kochens <ofwonoalex256@gmail.com>
2026-01-09 11:09:40 +0300

    feat: Learnt conditional rendering

--- src/components/starProject/Star.jsx
+++ src/components/starProject/Star.jsx
@@ -5,16 +5,22 @@ const Star = () => {
   const [hover, setHover] = useState(0);
   const stars = [1, 2, 3, 4, 5];
   return (
-    <div className='paragraph'>
+    <div className='container'>
       <p className='h1style'>Rate your Experience</p>
-
       <ul>
         {stars.map((star) => (
           <span
+            key={star}
             onClick={() => setRating(star)}
             onMouseEnter={() => setHover(star)}
-            key={star}
-            className='star-span'
+            onMouseLeave={() => setHover(0)}
+            style={{
+              color: star <= (hover || rating) ? 'gold' : 'gray',
+              fontSize: '32px',
+              cursor: 'pointer',
+              margin: '5px',
+            }}
+            className='star'
           >
             {'\u2605'}
           </span>
