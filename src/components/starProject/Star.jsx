f246106a333aad7d616adb57435b458079681a1c
alex Kochens <ofwonoalex256@gmail.com>
2026-01-17 01:25:45 +0300

    Learnt More on component composition

--- src/components/starProject/Star.jsx
+++ src/components/starProject/Star.jsx
@@ -3,7 +3,18 @@ import StarComponent from './StarComponent';
 const Star = ({ heading, feedbackMessages }) => {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
+  const [submitted, setSubmitted] = useState(false);
   const stars = [1, 2, 3, 4, 5];
+  const handleSubmission = () => {
+    if (rating > 0) {
+      setSubmitted(true);
+    }
+  };
+  const closeModal = () => {
+    setSubmitted(false);
+    setHover(0);
+    setRating(0);
+  };
   return (
     <div className='container'>
       <p className='h1style'>{heading}</p>
@@ -18,28 +29,34 @@ const Star = ({ heading, feedbackMessages }) => {
             hoverClick={setHover}
             hoverLeave={() => setHover(null)}
           />
-
-          // <span
-          //   key={star}
-          //   onClick={() => setRating(star)}
-          //   onMouseEnter={() => setHover(star)}
-          //   onMouseLeave={() => setHover(0)}
-          //   style={{
-          //     color: star <= (hover || rating) ? 'gold' : 'gray',
-          //     fontSize: '32px',
-          //     cursor: 'pointer',
-          //     margin: '5px',
-          //   }}
-          //   className='star'
-          // >
-          //   {'\u2605'}
-          // </span>
         ))}
       </ul>
 
       {rating > 0 && (
         <p className='feedbackMessages'>{feedbackMessages[rating - 1]}</p>
       )}
+      <button
+        onClick={handleSubmission}
+        className='submit-btn'
+        disabled={rating === 0}
+      >
+        Submit{' '}
+      </button>
+      {/* Modal */}
+
+      {
+        <div className='modal-overly'>
+          <div className='modal'>
+            <h2>Thank you</h2>
+            <p>
+              You rated us {rating} star{rating > 1 ? 's' : ''}{' '}
+            </p>
+            <button onClick={closeModal} className='close-btn'>
+              Close
+            </button>
+          </div>
+        </div>
+      }
     </div>
   );
 };
