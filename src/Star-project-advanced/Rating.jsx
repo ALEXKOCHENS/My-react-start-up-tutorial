165ac3f52485420167532a1be18e35d3e20710dc
alex Kochens <ofwonoalex256@gmail.com>
2026-01-19 02:07:45 +0300

    added hover state

--- src/Star-project-advanced/Rating.jsx
+++ src/Star-project-advanced/Rating.jsx
@@ -4,6 +4,7 @@ import Star from './Star';
 const Rating = ({ ProjectHeading }) => {
   const stars = [1, 2, 3, 4, 5];
   const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
   const resetUI = () => {
     setRating(0);
   };
@@ -13,13 +14,15 @@ const Rating = ({ ProjectHeading }) => {
       <div className='container'>
         {stars.map((star) => (
           <Star
+            hover={hover}
             key={star}
             star={star}
             rating={rating}
             ratingClick={() => setRating(star)}
+            hoverClick={() => setHover(star)}
           />
         ))}
-        <p>
+        <p className='rating-paragraph'>
           You rated {rating} star{rating > 1 ? 's' : ''}
         </p>
         <button className='reset-btn' onClick={resetUI}>
--- src/Star-project-advanced/Star.jsx
+++ src/Star-project-advanced/Star.jsx
@@ -1,10 +1,18 @@
-const Star = ({ rating, star, ratingClick, color = 'gold' }) => {
+const Star = ({
+  rating,
+  hover,
+  star,
+  ratingClick,
+  hoverClick,
+  color = 'gold',
+}) => {
   return (
     <span
       className='star'
       onClick={ratingClick}
+      onMouseEnter={hoverClick}
       style={{
-        color: star <= rating ? color : '#ccc',
+        color: star <= (rating || hover) ? color : '#ccc',
       }}
     >
       {'\u2605'}
